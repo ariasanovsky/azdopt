949f8e9467d93da3795c9a858b52c28b17b45e14
Alex Riasanovsky <a.riasanovsky@gmail.com>
2023-11-18 06:56:44 -0500

    forgot to remove this file

--- graph-state/src/simple_graph/tree/display.rs
+++
@@ -1,7 +0,0 @@
-use super::PrueferCode;
-
-impl<const N: usize> core::fmt::Display for PrueferCode<N> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
-    }
-}
